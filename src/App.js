c5b03c03645daad25843bdd80c549f52c8ebcaf2
Naufal Prima Yoriko <primayoriko@gmail.com>
2020-05-31 22:33:12 +0700

    add some form funcionalities

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import SortOptions from './SortOptions';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( 
+        <div className = "main">
+            <div className = "mainHeader">
+                <p> Sorting Simulator </p>
+                <p> Place to simulate and understand sorting better... </p> 
+            </div>
+            <div className = "mainBody"> 
+                <SortOptions />
+            </div>
+        </div>
+    );
 }
 
 export default App;
+
+// <div className="App">
+        //   <header className="App-header">
+        //     <img src={logo} className="App-logo" alt="logo" />
+        //     <p>
+        //       Edit <code>src/App.js</code> and save to reload.
+        //     </p>
+        //     <a
+        //       className="App-link"
+        //       href="https://reactjs.org"
+        //       target="_blank"
+        //       rel="noopener noreferrer"
+        //     >
+        //       Learn React
+        //     </a>
+        //   </header>
+        // </div>
---
+++ src/test.js
@@ -0,0 +1,1 @@
+console.log([...Array(0).keys()])
