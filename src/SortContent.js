b8961f12ee353418fd911b5cd480268395cb19b9
Naufal Prima Yoriko <primayoriko@gmail.com>
2020-06-01 19:44:43 +0700

    add some alignment with css

--- src/SortContent.js
+++ src/SortContent.js
@@ -4,7 +4,22 @@ import './SortContent.css';
 class SortOptions extends React.Component{
     constructor(props){
         super(props);
-        this.state = {};
+        this.state = {values: [], description:""};
+        this.handleValueChange = this.handleValueChange.bind(this);
+        this.handleSortChange = this.handleSortChange.bind(this);
+    }
+
+    handleValueChange(event){
+        this.setState({values: event.target.value.split(";").map((x) => {
+            return parseInt(x, 10);
+            }).filter((x) =>{
+                return x > 0 && x < 1000;
+            })
+        })
+    }
+
+    handleSortChange(event){
+
     }
 
     animate(){
@@ -17,45 +32,64 @@ class SortOptions extends React.Component{
                 <table>
                     <tbody>
                         <tr>
-                            <td colSpan={2}>
+                            <td colSpan={3}>
                                 Input value to be sorted: 
                             </td>
                         </tr>
                         <tr>
-                            <td colSpan={2}> 
-                                <font color="#CFAFAF">
-                                    (Separate it by a ';', recommended below 16 num)
+                            <td colSpan={3}> 
+                                <font color="#CFAFAF" style={{fontSize: "10.5px"}}>
+                                    (Separate it by a <b>';'</b>. It's recommended to insert at max. 10 numbers,
                                 </font>
                             </td>
                         </tr>
                         <tr>
-                            <td colSpan={2}>
-                                <input size="60" name="values" />
+                            <td colSpan={3}> 
+                                <font color="#CFAFAF" style={{fontSize: "10.5px"}}>
+                                    and  please insert value between 1 and 999, inclusive.)
+                                </font>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colSpan={3}>
+                                <input size="60" name="values" onChange={this.handleValueChange} />
                             </td>
                         </tr>
                         <tr>
                             <td>Sorting Type:</td>
                             <td>
-                                <input  type="radio" name="count" />
+                                <input colSpan={2} type="radio" name="count" />
                                 Bubble Sort
                             </td>
                         </tr>
                         <tr>
                             <td></td>
                             <td>
-                                <input  type="radio" name="count" />
+                                <input colSpan={2}  type="radio" name="count" />
                                 Merge Sort
                             </td>
                         </tr>
                         <tr>
-                            <td colSpan={2}>
+                            <td colSpan={3} style={{textAlign: "center"}}>
+                                <div className="sortDesc">
+                                    {this.state.description}
+                                </div> 
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colSpan={3} style={{textAlign: "center"}}>
                                 <button className="animateButton" size="60" onClick={() => this.animate()}> 
                                     Simulate! </button> 
                             </td>
-                            <td></td>
                         </tr>
                     </tbody>
                 </table>
+                {/* <p> tHIS IS THE VALUES: </p>
+                {
+                    this.state.values.map((x) =>{
+                        return <p> {x} </p>;
+                    })
+                } */}
             </form>
         );
         return sortOptions;
@@ -65,6 +99,7 @@ class SortOptions extends React.Component{
 class Animation extends React.Component{
     constructor(props){
         super(props);
+        this.state = {values: [123,12,2,21,23,223,1,2,21,123,12,213,1]}
 
     }
 
@@ -79,14 +114,14 @@ class Animation extends React.Component{
 class SortContent extends React.Component {
     constructor(props){
         super(props);
-
     }
 
     render(){
         return (
-            <div>
+            <div className="contents">
+                {/* <p>asjdhasld</p> */}
                 <SortOptions />
-                <Animation />
+                <Animation />       
             </div>
         );
     }
